fa377be8ba36ecc9f175552145dcddaa2589daed
Maziar Sojoudian <sojodyan@gmail.com>
2019-05-08 23:08:37 -0400

    add dynamic port scanner

---
+++ socket_2.py
@@ -0,0 +1,15 @@
+import socket
+
+host = input("Host is :")
+port = int(input("Port number to scan is :"))
+
+for port in range(port):
+    try:
+        s = socket.socket()
+        s.settimeout(2)
+        print(port)
+        s.connect((host, port))
+        print(s.recv(1024))
+        s.close()
+    except socket.error as error:
+        print(error)
